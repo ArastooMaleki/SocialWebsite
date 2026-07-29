a80c56c680eb6adc788385c611e079be5416244c
Arastoo Maleki <arastoo.maleki.2003@gmail.com>
2026-07-29 18:12:57 +0330

    refactor : using django predefined login view
    -we use our custom log just like previous commit \

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
-from . import views
+from django.contrib.auth import views
+# from . import views
 
 urlpatterns = [
-    path("login/", views.user_login, name="login"),
+    path("login/", views.LoginView.as_view(), name="login"),
 ]
