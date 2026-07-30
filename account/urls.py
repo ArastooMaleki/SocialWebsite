050ec0f0c17aefd5b2dc764d2ce07b93e1ef9d57
Arastoo Maleki <arastoo.maleki.2003@gmail.com>
2026-07-30 17:03:35 +0330

    feat : create change password views

--- account/urls.py
+++ account/urls.py
@@ -6,5 +6,13 @@ from .views import dashoboard
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout", views.LogoutView.as_view(), name="logout"),
+    path(
+        "change-password/", views.PasswordChangeView.as_view(), name="password_change"
+    ),
+    path(
+        "change-password/done/",
+        views.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
     path("", dashoboard, name="dashboard"),
 ]
